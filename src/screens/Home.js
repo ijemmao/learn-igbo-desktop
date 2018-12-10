3e494e0ae32d0e7f384b27c1647d5997fa934715
ijemmao <ijemmao>
2018-12-10 15:29:48 -0800

    added a little effect to logo

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -37,7 +37,9 @@ export default class Home extends Component {
     if (this.state.renderPage) {
       return (
         <div className="home-container">
-          <h1 id="logo">Learn Igbo</h1>
+          <a href="/">
+            <h1 id="logo">Learn Igbo</h1>
+          </a>
           <h5>An interactive web app to teach the Naija language</h5>
           <div className="major-options-container">
             {this.majorOption('Camera', 'Interact with your camera and photos!')}
