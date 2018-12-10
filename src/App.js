9750d5c881694f643ed4cfce00f3237e9414098d
ijemmao <ijemmao>
2018-12-09 21:43:28 -0800

    included react router

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import Home from './screens/Home';
 import './styles/App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Home />
-      </div>
-    );
-  }
-}
+const App = () => (
+  <Router>
+    <Route path="/" exact component={Home} />
+  </Router>
+)
 
 export default App;
