79b8af7476835280cff163eca9282015b908c5cb
ijemmao <ijemmao>
2018-12-10 21:59:41 -0800

    formatted camera page

--- src/components/ImagePreview.js
+++ src/components/ImagePreview.js
@@ -1,6 +1,9 @@
-import React, { Component } from 'react';
-import Clarifai from 'clarifai';
-import './../styles/ImagePreview.css';
+import React, { Component } from 'react'
+import ReactLoading from 'react-loading'
+import Clarifai from 'clarifai'
+import Suggestion from './../components/Suggestion'
+import translate from './../actions/translate';
+import './../styles/ImagePreview.css'
 
 import env from './../env.json'
 
@@ -10,45 +13,80 @@ export default class ImagePreview extends Component {
     super(props)
 
     this.state = {
+      analyzingPhoto: false,
       imgBase64: null,
+      english: ['this', 'is', 'a', 'word', 'another', 'something'],
+      igbo: ['sith', 'si', 'a', 'drow', 'rehtona', 'gnihtemos'],
     }
   }
 
+  renderSuggestions = () => {
+    if (this.state.analyzingPhoto) {
+      return <ReactLoading className="loading" type={'spin'} color={'#ccc'} height={'10vh'} width={'10vh'} />
+    } else if (!this.state.analyzingPhoto && this.state.english.length > 0 && this.state.igbo.length > 0) {
+      return this.state.english.map((word, index) => {
+        return <Suggestion key={`${word}${index}`} english={word} igbo={this.state.igbo[index]} />
+      })
+    }
+  }
+    
   renderImage = (e) => {
     console.log(e.target.files);
     if (e.target.files && e.target.files[0]) {
       const reader = new FileReader();
 
       reader.onload = (e) => {
-        const imagePreview = document.querySelector('.image-preview')
+        const imagePreview = document.querySelector('.image-preview img')
         imagePreview.setAttribute('src', e.target.result)
         this.setState({ imgBase64: e.target.result.split(',')[1] })
+        this.sendImage(e.target.result.split(',')[1])
       }
 
       reader.readAsDataURL(e.target.files[0])
     }
   }
 
-  sendImage = () => {
+  sendImage = (image) => {
+    this.setState({ analyzingPhoto: true })
     app.models.initModel({ id: Clarifai.GENERAL_MODEL, version: 'aa7f35c01e0642fda5cf400f543e7c40' })
       .then(generalModel => {
-        return generalModel.predict(this.state.imgBase64);
+        return generalModel.predict(image)
       })
       .then((res) => {
         const concepts = res.outputs[0].data.concepts;
-        const suggestions = Array.from(new Set(concepts.map(concept => concept.name)));
-        console.log(suggestions);
+        const suggestions = Array.from(new Set(concepts.map(concept => concept.name)))
+        this.setState({ analyzingPhoto: false, english: suggestions })
+        translate.translateEnglish(suggestions).then((results) => {
+          this.setState({ igbo: results.words })
+        })
       })
       .catch((error) => {
         console.log(error)
       })
   }
+
+  clickInput = () => {
+    const imageInput = document.querySelector('input.image-input');
+    imageInput.click();
+  }
+
   render() {
-    if (this.state.imgBase64) this.sendImage()
     return (
       <div className="image-preview-container">
-        <input type="file" accept="image/png, image/jpeg" onChange={this.renderImage}/>
-        <img className="image-preview" src="/" />
+        <input className="image-input" type="file" accept="image/png, image/jpeg" onChange={this.renderImage}/>
+        <button className="choose-image" onClick={this.clickInput}>Upload Photo</button>
+        <div className="image-results-container">
+          <span className="image-preview">
+            <h3 className="image-result-header">Uploaded Photo</h3>
+            <img src="/" />
+          </span>
+          <div className="suggestions-container">
+            <h3 className="image-result-header">Detected Terms</h3>
+            <div className="suggestions-labels-container">
+              {this.renderSuggestions()}
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
--- src/screens/Camera.js
+++ src/screens/Camera.js
@@ -1,30 +1,8 @@
 import React, { Component } from 'react'
-import Accordion from './../components/Accordion'
-import ReactLoading from 'react-loading'
 import ImagePreview from './../components/ImagePreview'
 import './../styles/Camera.css'
 
 export default class Camera extends Component {
-
-  constructor(props) {
-    super(props)
-    this.state = {
-      english: [],
-      igbo: [],
-    }
-  }
-
-  updateEnglish = (english) => {
-    this.setState({ english })
-  }
-
-  renderSuggestions = () => {
-    if (this.state.english.length > 0) {
-      return this.state.english.map((word, index) => {
-       return  <Accordion key={`${word}${index}`} english={word} igbo={this.state.igbo[index]} />
-      })
-    } else return <ReactLoading className="loading" type={'spin'} color={'#ccc'} height={'10vh'} width={'10vh'} />
-  }
 
   render() {
     return (
@@ -32,8 +10,7 @@ export default class Camera extends Component {
         <h1>Upload A Photo</h1>
         <h5>Upload a photo to see what's in it!</h5>
         <div>
-          <ImagePreview updateEnglish={this.updateEnglish} />
-          {this.renderSuggestions()}
+          <ImagePreview />
         </div>
       </div>
     )
