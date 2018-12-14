6463901101dce339fdb918b52f63507eee159f75
ijemmao <ijemmao>
2018-12-13 19:30:46 -0800

    able to log in and out

--- src/actions/user.js
+++ src/actions/user.js
@@ -2,23 +2,20 @@ import firebase from 'firebase'
 const provider = new firebase.auth.GoogleAuthProvider()
 
 const createGoogleUser = () => {
-  firebase.auth().signInWithPopup(provider).then((result) => {
-    // This gives you a Google Access Token. You can use it to access the Google API.
-    let token = result.credential.accessToken;
-    // The signed-in user info.
-    let user = result.user;
-    console.log(user)
-  }).catch(function (error) {
-    // Handle Errors here.
-    let errorCode = error.code;
-    let errorMessage = error.message;
-    // The email of the user's account used.
-    let email = error.email;
-    // The firebase.auth.AuthCredential type that was used.
-    let credential = error.credential;
-
-    console.log('User:', email, 'was unable to sign up. Error Code:', errorCode, '. Error Message:', errorMessage)
-  });
+  return new Promise((resolve, reject) => {
+    firebase.auth().signInWithPopup(provider).then((result) => {
+      let token = result.credential.accessToken;
+      let user = result.user;
+      resolve(user)
+    }).catch(function (error) {
+      let errorCode = error.code;
+      let errorMessage = error.message;
+      let email = error.email;
+      let credential = error.credential;
+  
+      reject('User:', email, 'was unable to sign up. Error Code:', errorCode, '. Error Message:', errorMessage)
+    });
+  })
 }
 
 const getGoogleUser = () => {
@@ -27,10 +24,22 @@ const getGoogleUser = () => {
       if (user) {
         resolve(user)
       } else {
-        resolve({ displayName: '', email: '', photoURL: '', uri: ''})
+        resolve(null)
       }
     });
   })
 }
 
-export default { createGoogleUser, getGoogleUser }
+const signOutGoogleUser = () => {
+  return new Promise((resolve, reject) => {
+    firebase.auth().signOut().then(function () {
+      // Sign-out successful.
+      resolve(null)
+    }).catch(function (error) {
+      // An error happened.
+      reject(error)
+    });
+  })
+}
+
+export default { createGoogleUser, getGoogleUser, signOutGoogleUser }
--- src/components/UserAccount.js
+++ src/components/UserAccount.js
@@ -6,27 +6,38 @@ export default class UserAccount extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      name: '',
-      email: '',
-      photo: '',
-      uid: '',
+      name: null,
+      email: null,
+      photo: null,
+      uid: null,
       expanded: false
     }
   }
 
   componentWillMount = () => {
     user.getGoogleUser().then((res) => {
-      this.setState({
-        name: res.displayName,
-        email: res.email,
-        photo: res.photoURL,
-        uid: res.uid
-      })
+      if (res !== null) {
+        this.setState({
+          name: res.displayName,
+          email: res.email,
+          photo: res.photoURL,
+          uid: res.uid
+        })
+      }
     })
   }
 
   googleAccount = () => {
-    user.createGoogleUser()
+    user.createGoogleUser().then((res) => {
+      if (res !== null) {
+        this.setState({
+          name: res.displayName,
+          email: res.email,
+          photo: res.photoURL,
+          uid: res.uid
+        })
+      }
+    })
   }
 
   toggleMenu = () => {
@@ -40,15 +51,26 @@ export default class UserAccount extends Component {
     this.setState({ expanded: !this.state.expanded })
   }
 
+  signout = () => {
+    user.signOutGoogleUser().then((res) => {
+      this.setState({
+        name: null,
+        email: null,
+        photo: null,
+        uid: null,
+      })
+    })
+  }
+
   renderUserAccount = () => {
-    if (this.state.uid !== '') {
+    if (this.state.uid) {
       return (
         <span className="user-profile-container" onClick={this.toggleMenu}>
           <img className="user-profile-image" src={this.state.photo} />
           <h5>{this.state.name}</h5>
           <div className="dropdown-menu hidden">
             <h5>{this.state.email}</h5>
-            <button className="login-button logout">
+            <button className="login-button logout" onClick={this.signout}>
               Sign Out
             </button>
           </div>
