2e79054e29cb40d9192ee1e3aae4e6e0bf05180d
ijemmao <ijemmao>
2018-12-13 19:35:51 -0800

    removed comments

--- src/actions/firebase.js
+++ src/actions/firebase.js
@@ -1,11 +1,6 @@
 import * as firebase from 'firebase'
 import serviceAccount from './../assets/data/firebase.json'
 
-// admin.initializeApp({
-//   credential: admin.credential.cert(serviceAccount),
-//   databaseURL: "https://learn-igbo-1543470589176.firebaseio.com"
-// })
-
 firebase.initializeApp(serviceAccount)
 
 export default firebase
