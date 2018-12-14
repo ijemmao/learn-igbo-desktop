18189aa7517bd972560fbb7c9dbca5fa482afb26
ijemmao <ijemmao>
2018-12-13 17:31:12 -0800

    styled buttons

--- src/components/UserAccount.js
+++ src/components/UserAccount.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react'
+import './../styles/UserAccount.css'
 
 export default class UserAccount extends Component {
   render() {
     return (
-      <div>
-        <button>sign in</button>
-        <button>sign up</button>
+      <div className="user-account-container">
+        <button className="login-button">Sign In</button>
+        <button className="login-button sign-up">Sign Up</button>
       </div>
     )
   }
