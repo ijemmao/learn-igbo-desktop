714505d90531383d351eb80d0f564a17fb4d3fff
ijemmao <ijemmao>
2018-12-25 00:24:37 -0800

    included animation

--- src/screens/NoMatch.js
+++ src/screens/NoMatch.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import Anime from 'react-anime'
 import { NavLink } from 'react-router-dom'
 import '../styles/NoMatch.css'
 
@@ -11,14 +12,21 @@ export default class NoMatch extends Component {
   render() {
     return (
       <div className="no-match-container">
-        <h1><span className="uh-oh" role="img" aria-label="uh-oh">😨</span></h1>
-        <h1>Uh Oh! It seems there's some trouble</h1>
-        <h1>
-          Let's head back
-          <NavLink to="/" className="link-home">
-            {' home'}
-          </NavLink>
-        </h1>
+        <Anime
+          easing="easeOutElastic"
+          duration={2700}
+          delay={() => 1 * 240}
+          translateY={['100vh', '4vh']}
+        >
+          <h1><span className="uh-oh" role="img" aria-label="uh-oh">😨</span></h1>
+          <h1>Uh Oh! It seems there's some trouble</h1>
+          <h1>
+            Let's head back
+            <NavLink to="/" className="link-home">
+              {' home'}
+            </NavLink>
+          </h1>
+        </Anime>
       </div>
     )
   }
